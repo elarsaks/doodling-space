c7e90a91307cca35bcde9bc5259d87f5387177a8
Elar Saks <elarsaks@gmail.com>
2023-08-22 18:09:53 +0300

    Add logo.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Doodle from './components/Doodle';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="App">
       
-
+      <Logo />
       <Doodle />
     </div>
   );
--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const CornerDiv = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 50px;  // You can adjust the size as needed
-  height: 50px; // You can adjust the size as needed
-  background-color: #333; // You can adjust the color as needed
-  color: #fff; // Text color, adjust as needed
+  top: 1vh;
+  left: 1vw;
+  font-size: 1.5em;
+  font-weight: bold;
+  padding: 0.5em;
+  border-radius: 0.5em;
+  background-color: #333;
+  color: #fff;
 `;
 
 const UpperLeftCornerComponent: React.FC = () => {
-  return <CornerDiv>Content</CornerDiv>;
+  return <CornerDiv>Doodle Tool</CornerDiv>;
 };
 
 export default UpperLeftCornerComponent;
