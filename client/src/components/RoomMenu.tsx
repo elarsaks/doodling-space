a36897dfe1f13e2c8c059cb94c30f73b72cbfbb5
Elar Saks <elarsaks@gmail.com>
2023-08-24 17:19:37 +0300

    Update room menu.

--- client/src/components/RoomMenu.tsx
+++ client/src/components/RoomMenu.tsx
@@ -32,7 +32,7 @@ const Overlay = styled.div<{ show: boolean }>`
   z-index: 1;
 `;
 
-const StyledButton = styled.button<{ center: boolean }>`
+const StyledSelect = styled.select<{ center: boolean }>`
   padding: 0.5em 1em;
   border: 1px solid #fff;
   border-radius: 0.3em;
@@ -47,7 +47,7 @@ const StyledButton = styled.button<{ center: boolean }>`
   }
 `;
 
-const StyledSelect = styled.select`
+const StyledInput = styled.input<{ center: boolean }>`
   padding: 0.5em 1em;
   border: 1px solid #fff;
   border-radius: 0.3em;
@@ -55,24 +55,40 @@ const StyledSelect = styled.select`
   color: #fff;
   cursor: pointer;
   transition: background-color 0.3s;
+  font-size: ${({ center }) => (center ? "0.8em" : "0.5em")};
+  margin-top: 0.5em; // To give some space between the select and the input
 
   &:hover {
     background-color: rgba(1, 14, 33, 0.7);
   }
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.7);
+  }
 `;
 
 const existingRooms = [
-  { id: "hash1", name: "Room 1" },
-  { id: "hash2", name: "Room 2" },
-  { id: "hash3", name: "Room 3" },
+  // { id: "hash1", name: "Room 1" },
+  // { id: "hash2", name: "Room 2" },
+  // { id: "hash3", name: "Room 3" },
+  { id: "NEW_ROOM", name: "Create New Room" },
+  { id: "JOIN_ROOM", name: "Join a Room" },
 ];
 
 const RoomMenu: React.FC = () => {
   const [selectedRoom, setSelectedRoom] = useState<string>("");
   const [isNewRoomCreated, setIsNewRoomCreated] = useState<boolean>(false);
+  const [showJoinInput, setShowJoinInput] = useState<boolean>(false);
 
   const handleRoomChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedRoom(event.target.value);
+    if (event.target.value === "NEW_ROOM") {
+      handleCreateRoom();
+    } else if (event.target.value === "JOIN_ROOM") {
+      setShowJoinInput(true);
+    } else {
+      setShowJoinInput(false);
+    }
   };
 
   const handleCreateRoom = () => {
@@ -85,25 +101,30 @@ const RoomMenu: React.FC = () => {
       <Overlay show={!isNewRoomCreated} />
       <RoomMenuContainer center={!isNewRoomCreated}>
         <div> D O O D L E </div>
-        {isNewRoomCreated && (
-          <StyledSelect value={selectedRoom} onChange={handleRoomChange}>
-            <option value="" disabled>
-              Select an existing room
-            </option>
 
-            {existingRooms.map((room) => (
-              <option key={room.id} value={room.id}>
-                {room.name}
-              </option>
-            ))}
-          </StyledSelect>
+        <StyledSelect
+          value={selectedRoom}
+          center={!isNewRoomCreated}
+          onChange={handleRoomChange}
+        >
+          <option value="" disabled>
+            Select a room
+          </option>
+
+          {existingRooms.map((room) => (
+            <option key={room.id} value={room.id}>
+              {room.name}
+            </option>
+          ))}
+        </StyledSelect>
+
+        {showJoinInput && (
+          <StyledInput
+            type="text"
+            placeholder="Enter room ID to join"
+            center={!isNewRoomCreated}
+          />
         )}
-        <StyledButton center={!isNewRoomCreated} onClick={handleCreateRoom}>
-          Create New Room
-        </StyledButton>
-        <StyledButton center={!isNewRoomCreated} onClick={handleCreateRoom}>
-          Join a Room
-        </StyledButton>
       </RoomMenuContainer>
     </>
   );
