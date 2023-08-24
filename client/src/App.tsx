55618eddc00a664c0aa5d6f8d4aadd051f1a7500
Elar Saks <elarsaks@gmail.com>
2023-08-24 16:16:48 +0300

    Delete logo.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import Doodle from "./components/Doodle";
-import Logo from "./components/Logo";
 import Menu from "./components/Menu";
 import RoomMenu from "./components/RoomMenu";
 
 function App() {
   return (
     <div className="App">
-      {/* <Logo /> */}
       <RoomMenu />
       <Menu />
       <Doodle />
