6791982f366b3ab165212d1d92035811995f1cee
Elar Saks <elarsaks@gmail.com>
2023-08-24 16:33:51 +0300

    Install icons.

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUndo,
+  faRedo,
+  faBan,
+  faHandPaper,
+  faPencilAlt,
+  faEraser,
+  faPalette,
+} from "@fortawesome/free-solid-svg-icons";
 
 const MenuDiv = styled.div`
   position: absolute;
@@ -19,7 +29,7 @@ const MenuDiv = styled.div`
   @media (max-width: 768px) {
     right: -95%;
     bottom: 10%;
-    transform: translateX(0%);
+    transform: translateX(0%) translateY(-50%);
   }
 `;
 
@@ -32,6 +42,7 @@ const MenuButton = styled.button`
   color: #fff;
   cursor: pointer;
   transition: background-color 0.3s;
+  font-size: 1.2em; /* Adjust this to change the icon size */
 
   &:hover {
     background-color: #777;
@@ -41,12 +52,27 @@ const MenuButton = styled.button`
 const RightCenterMenu: React.FC = () => {
   return (
     <MenuDiv>
-      <MenuButton>Undo</MenuButton>
-      <MenuButton>Redo</MenuButton>
-      <MenuButton>Clear</MenuButton>
-      <MenuButton>Pencil</MenuButton>
-      <MenuButton>Eraser</MenuButton>
-      <MenuButton>Color</MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faUndo} />
+      </MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faRedo} />
+      </MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faBan} />
+      </MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faHandPaper} />
+      </MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faPencilAlt} />
+      </MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faEraser} />
+      </MenuButton>
+      <MenuButton>
+        <FontAwesomeIcon icon={faPalette} />
+      </MenuButton>
     </MenuDiv>
   );
 };
